a8c7c8116c7231549a3d7a049389521fd98e5aac
daviel <davidkrawiec@posteo.net>
2024-10-18 17:58:14 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CONFIG_DIR="/opt/pigo/pigo-config"
+
+sudo su -
+
+
+if [ ! -d "$CONFIG_DIR" ] ; then
+    git clone "https://github.com/daviel/pigo-config" "$CONFIG_DIR"
+    cd "$CONFIG_DIR"
+    ./install.sh
+else
+    # Update pigo-config
+    cd "$CONFIG_DIR"
+
+    # TODO: implement version comparison
+    # CONFIG_VERS=`cat version | tr -d '\n'`
+    
+    git reset --hard
+    git pull
+    ./install.sh
+fi
