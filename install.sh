0d6543fcad9cbdef4cb222ef7585f68f5884c0a6
daviel <davidkrawiec@posteo.net>
2026-05-15 19:04:10 +0200

    Refactor install.sh for improved package management
    Updated install script to include wget for SDL2 and WiringPi installation, removed unnecessary service commands, and cleaned up package installations.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ KEYMAPPER_DIR="/opt/pigo/keymapper"
 
 apt update
 apt dist-upgrade -y
-apt install -y libevdev-dev git python3-pip libcurl4-openssl-dev libopenal1 libmodplug1 libvorbisfile3 libtheora0 libmpg123-0 python3-libevdev
+apt install -y libevdev-dev git python3-pip libcurl4-openssl-dev libopenal1 libmodplug1 libvorbisfile3 libtheora0 libmpg123-0 python3-libevdev wget
 
 mkdir -p /opt/pigo/games
 
@@ -48,57 +48,34 @@ done
 echo "" >> /boot/firmware/cmdline.txt
 
 
-cp $CONFIG_DIR/buster.list /etc/apt/sources.list.d/buster.list
-cp $CONFIG_DIR/bullseye.list /etc/apt/sources.list.d/bullseye.list
 cp $CONFIG_DIR/config.txt /boot/firmware/config.txt
 cp $CONFIG_DIR/asound.conf /etc/asound.conf
-cp $CONFIG_DIR/fbcp.service /etc/systemd/system/fbcp.service
 cp $CONFIG_DIR/lightdisplay.service /etc/systemd/system/lightdisplay.service
 cp $CONFIG_DIR/pigogui.service /etc/systemd/system/pigogui.service
 
 systemctl daemon-reload
 systemctl disable getty@tty1.service
 systemctl disable userconfig.service
-systemctl disable hciuart.service
 systemctl disable keyboard-setup.service
 systemctl disable console-setup.service
-systemctl disable bluetooth.service
 
 systemctl enable pigogui.service
 systemctl enable lightdisplay.service
-systemctl enable fbcp.service
-
-systemctl stop pigogui.service
-systemctl stop lightdisplay.service
-systemctl stop fbcp.service
-systemctl stop userconfig.service 
-
-
-rm -f /usr/lib/libSDL2*
-rm -f /lib/arm-linux-gnueabihf/libSDL2*
-
-wget https://github.com/daviel/SDL-pigo/releases/download/2.0.10-pigo/libSDL2-2.0.so.0.10.0 -O /usr/lib/libSDL2-2.0.so
-cp /usr/lib/libSDL2-2.0.so /lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.10.0
 
 rm -f /usr/bin/micropython
-rm -f /usr/bin/fbcp
-
 wget https://github.com/daviel/lvgl/releases/download/v9.3.0/micropython -O /usr/bin/micropython
-wget https://github.com/daviel/fbcp-ili9341-pigo/releases/download/v1.0-pigo/fbcp-ili9341 -O /usr/bin/fbcp
-
-
 
 ldconfig
 chmod +x /usr/bin/micropython
-chmod +x /usr/bin/fbcp
 
 systemctl restart pigogui.service
 systemctl restart lightdisplay.service
-systemctl restart fbcp.service
 
-apt update
-apt install libraspberrypi-dev/buster libraspberrypi0/buster raspberrypi-bootloader/buster wiringpi -y --allow-downgrades
+# wiring pi
+wget https://github.com/WiringPi/WiringPi/releases/download/3.18/wiringpi_3.18_armhf.deb
+dpkg -i wiringpi_3.18_armhf.deb
 
+# cleanup
 apt remove -y triggerhappy firmware-atheros firmware-libertas gcc-12 g++-12 cpp-12 gdb firmware-misc-nonfree manpages-dev firmware-realtek manpages-dev manpages iso-codes libicu* nfs-common
 apt autoremove -y
 apt clean all
